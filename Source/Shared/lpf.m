858cd578f4b91cce26e9d7019684322f5f23d68b
John <johnkc1992@gmail.com>
2017-11-14 16:27:33 -0700

    Scale the lpf by 3 so match the output amplitude

--- Source/Shared/lpf.m
+++ Source/Shared/lpf.m
@@ -1,4 +1,8 @@
-fid = fopen('lpf.bin', 'wb');
+fid = fopen('lpf_scaled.bin', 'wb');
 fwrite(fid, size(lpf, 2), 'int');
 fwrite(fid, lpf, 'float');
+fclose(fid);
+
+fid = fopen('digInterp.bin');
+y = fread(fid, 'float');
 fclose(fid);
